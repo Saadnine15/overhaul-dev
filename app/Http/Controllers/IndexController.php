bffdacabad61ad898f65b6115c4dbfa92e7aea35
Saud S <saud@nine15.com>
2016-11-29 22:30:49 +0500

    First Commit

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -76,7 +76,8 @@ class IndexController extends ShopifyAppInstallationBaseController
     }
 
     public function productsdb(){
-        return ProductModel::where('store_url', 'test-shop-368.myshopify.com')->pluck('product_id', 'handle');
+        $result = ProductModel::where('store_url', 'test-shop-368.myshopify.com')->pluck('product_id', 'handle');
+        echo gettype($result);
     }
 
     public function jobsFailed(){
