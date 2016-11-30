05773c73c6de981f4e04c2da20f43d0b0395d7a0
Saud S <saud@nine15.com>
2016-11-30 14:50:22 +0500

    First Commit

--- config/app.php
+++ config/app.php
@@ -160,6 +160,8 @@ return [
 
         JeroenG\Packager\PackagerServiceProvider::class,
 
+        Maatwebsite\Excel\ExcelServiceProvider::class,
+
     ],
 
     /*
@@ -205,7 +207,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
+        'Excel' => Maatwebsite\Excel\Facades\Excel::class,
     ],
 
 ];
