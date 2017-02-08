975c48454c18f001f85c6da840b9cc4534d4ac4a
Saud S <saud@nine15.com>
2017-02-09 03:27:45 +0500

    First Commit

--- public/assets/admin/js/angular/app.js
+++ public/assets/admin/js/angular/app.js
@@ -141,8 +141,61 @@ var angularApp = angular.module('product-updating-app', included_modules)
         // Function to convert to JSON
         var convertToJSON = function (content) {
 
+            var strData = content;
+
+            // Check to see if the delimiter is defined. If not,
+            // then default to comma.
+            //strDelimiter = (strDelimiter || ",");
+            var strDelimiter = ",";
+            // Create a regular expression to parse the CSV values.
+            var objPattern = new RegExp((
+                // Delimiters.
+            "(\\" + strDelimiter + "|\\r?\\n|\\r|^)" +
+            // Quoted fields.
+            "(?:\"([^\"]*(?:\"\"[^\"]*)*)\"|" +
+            // Standard fields.
+            "([^\"\\" + strDelimiter + "\\r\\n]*))"), "gi");
+            // Create an array to hold our data. Give the array
+            // a default empty first row.
+            var arrData = [[]];
+            // Create an array to hold our individual pattern
+            // matching groups.
+            var arrMatches = null;
+            // Keep looping over the regular expression matches
+            // until we can no longer find a match.
+            while (arrMatches = objPattern.exec(strData)) {
+                // Get the delimiter that was found.
+                var strMatchedDelimiter = arrMatches[1];
+                // Check to see if the given delimiter has a length
+                // (is not the start of string) and if it matches
+                // field delimiter. If id does not, then we know
+                // that this delimiter is a row delimiter.
+                if (strMatchedDelimiter.length && (strMatchedDelimiter != strDelimiter)) {
+                    // Since we have reached a new row of data,
+                    // add an empty row to our data array.
+                    arrData.push([]);
+                }
+                // Now that we have our delimiter out of the way,
+                // let's check to see which kind of value we
+                // captured (quoted or unquoted).
+                if (arrMatches[2]) {
+                    // We found a quoted value. When we capture
+                    // this value, unescape any double quotes.
+                    var strMatchedValue = arrMatches[2].replace(
+                        new RegExp("\"\"", "g"), "\"");
+                } else {
+                    // We found a non-quoted value.
+                    var strMatchedValue = arrMatches[3];
+                }
+                // Now that we have our value string, let's add
+                // it to the data array.
+                arrData[arrData.length - 1].push(strMatchedValue);
+            }
+            // Return the parsed data.
+            return (arrData);
+
             // Declare our variables
-            var lines = content.csv.split('\n'),
+            /*var lines = content.csv.split('\n'),
                 headers = lines[0].split(content.separator),
                 columnCount = lines[0].split(content.separator).length,
                 results = [];
@@ -158,8 +211,8 @@ var angularApp = angular.module('product-updating-app', included_modules)
                 var obj = {};
 
                 // Get our current line
-                var line = lines[i].split(new RegExp(content.separator + '(?:,?(".+?"))|(?:,?(.+?),?)'));
-                //var line = lines[i].split(new RegExp(content.separator + '(?:^|,)(?=[^"]|(")?)"?((?(1)[^"]*|[^,"]*))"?(?=,|$)'));
+                //var line = lines[i].split(new RegExp(content.separator + '(?:,?(".+?"))|(?:,?(.+?),?)'));
+                var line = lines[i].split(new RegExp(content.separator + '(?:^|,)(?=[^"]|(")?)"?((?(1)[^"]*|[^,"]*))"?(?=,|$)'));
                 //var line = lines[i].split(new RegExp(content.separator + '(?![^"]*"(?:(?:[^"]*"){2})*[^"]*$)'));
                 console.log('line', line);
                 // For each header
@@ -174,7 +227,7 @@ var angularApp = angular.module('product-updating-app', included_modules)
             }
 
             // Return our array
-            return results;
+            return results;*/
         };
 
         return {
@@ -284,5 +337,4 @@ var angularApp = angular.module('product-updating-app', included_modules)
             ShopifyApp.flashNotice(message);
         }
     });
-
 
