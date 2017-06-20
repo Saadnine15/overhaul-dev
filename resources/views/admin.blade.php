d7d9ad7a20d920b336fd408e6e95687c5fe400ac
Saad <saad@nine15.com>
2017-06-20 14:52:43 -0500

    better

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -79,7 +79,7 @@
                                 </tr>
                                 </thead>
                                 <tbody>
-                                <tr ng-repeat="row in table track by $index">
+                                <tr ng-repeat="row in table">
                                     <td  ng-repeat="headerOption in headerOptions.offered">@{{ row[headerOption.key] }}</td>
                                 </tr>
                                 </tbody>
