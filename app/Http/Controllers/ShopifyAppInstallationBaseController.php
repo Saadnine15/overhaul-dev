714a94dfe8519c6b19fc2dec940a09b5b5818e6c
Saad <saad@nine15.com>
2017-06-20 08:34:09 -0500

    better

--- app/Http/Controllers/ShopifyAppInstallationBaseController.php
+++ app/Http/Controllers/ShopifyAppInstallationBaseController.php
@@ -55,7 +55,7 @@ class ShopifyAppInstallationBaseController extends ShopifyAppBaseController
     }
 
     private function _addNewStoreSettings(){
-        echo $this->store;die;
+
         //get permanent access token
         $access_token = $this->shopify_app_helper->oauthAccessToken(
             $this->store, $this->app_settings->api_key, $this->app_settings->shared_secret, Request::input('code')
