b003308da52d20aff58f51aef6fd4e4c872b5890
Saad <saad@nine15.com>
2017-06-28 10:29:24 -0500

    make better

--- public/assets/admin/js/angular/app.js
+++ public/assets/admin/js/angular/app.js
@@ -79,11 +79,14 @@ var angularApp = angular.module('product-updating-app', included_modules)
                                 if(csv_row[option.mapped_to]!=''){
                                     obj[option.key] = csv_row[option.mapped_to];
                                     console.log(option.mapped_to, csv_row[option.mapped_to]);
+                                }else if(csv_row[option.mapped_to]=='Variant Sku'){
+                                        alert('Variant Sku missing on line '+k)
                                 }
 
 
 
                             }
+
                         }
                     });
                     if(Object.keys(obj).length > 0){
