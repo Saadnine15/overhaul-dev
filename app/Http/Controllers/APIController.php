dbec05beb0284fbdf8cd517ac3b381bfeec44c0e
Saad <saad@nine15.com>
2017-06-29 09:56:20 -0500

    better

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -69,8 +69,7 @@ class APIController extends ShopifyApiBaseController {
 
             $job = new ProductsUpdater($store_settings, $shopify_request_param_arr);
 
-            $this->dispatch($job);
-            return 11;
+//            $this->dispatch($job);
             return $shopify_request_param_arr;
         }
         else{
