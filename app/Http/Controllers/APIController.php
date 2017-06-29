16d1319a24371b19660b298ba12dc2ac35fc1b28
Saad <saad@nine15.com>
2017-06-29 13:13:25 -0500

    better

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -25,7 +25,7 @@ class APIController extends ShopifyApiBaseController {
 
 
 
-
+        return $variant_sku_array;
         if($variant_sku_array){
             foreach($variant_sku_array as $item){
                 if($item==''){
