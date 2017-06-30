abf274b69be44cac6c25636db9ddee99399a4b7e
Saad <saad@nine15.com>
2017-06-30 11:41:37 -0500

    better

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -39,11 +39,7 @@
 
                 <div class="Polaris-Layout__AnnotatedSection">
                     <div class="Polaris-Layout__AnnotationWrapper">
-                        <div class="Polaris-Layout__Annotation">
-                            <div class="Polaris-TextContainer">
-                                <h2 class="Polaris-Heading">File</h2>
-                            </div>
-                        </div>
+                        <h2 class="Polaris-Heading">Upload your CSV</h2>
                         <div class="Polaris-Layout__AnnotationContent">
                             <div class="Polaris-Card">
                                 <div class="Polaris-Card__Section">
