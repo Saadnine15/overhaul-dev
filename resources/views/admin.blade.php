87963d9c04e62c28ff45fa0ee39fc18958763e62
Saad <saad@nine15.com>
2017-07-19 08:37:13 -0500

    better

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -22,7 +22,7 @@
                 </div>
                 <div class="row w-row">
                     <div class="col-pad-fix-left w-col w-col-3 w-col-stack">
-                        <div class="opacity-overlay"></div>
+                        <div ng-show="fileSelected==false"   class="opacity-overlay"></div>
                         <div class="step-circle">
                             <div>2</div>
                         </div>
@@ -31,12 +31,10 @@
                                 <div ng-if="$first"   ng-repeat="headerOption in headerOptions.offered">
                                     <div class="upload-title">@{{ headerOption.value }} <span class="highlight-text">(required)</span></div>
                                     <div class="w-form">
-                                <form id="email-form">
                                     <select class="select-field w-select"  aria-invalid="false" ng-model="headerOption.mapped_to" ng-change="updateTable(headerOption)" >
                                         <option label="Select" value="__placeholder__" disabled="" hidden=""></option>
                                         <option ng-repeat="(key, value) in headerOptions.inCSV" value="@{{ key }}">@{{ value }}</option>
                                     </select>
-                                </form>
                                 <div class="w-form-done">
                                     <div>Thank you! Your submission has been received!</div>
                                 </div>
@@ -59,13 +57,11 @@
                                         <div class="block-padding">
                                             <div class="upload-title">@{{ headerOption.value }}</div>
                                             <div class="w-form">
-                                                <form  id="email-form" >
                                                     <select class="select-field w-select" aria-invalid="false" ng-model="headerOption.mapped_to" ng-change="updateTable(headerOption)" >
                                                         <option label="Select" value="__placeholder__" disabled="" hidden=""></option>
                                                         <option ng-repeat="(key, value) in headerOptions.inCSV" value="@{{ key }}">@{{ value }}</option>
                                                     </select>
 
-                                                </form>
                                                 <div class="w-form-done">
                                                     <div>Thank you! Your submission has been received!</div>
                                                 </div>
@@ -79,7 +75,29 @@
                                 </div>
                             </div>
                         </div>
-                        <div class="opacity-overlay"></div>
+                        <div ng-show="skuSelected==false" class="opacity-overlay"></div>
+                    </div>
+                    <div ng-show="showTable==true" class="results selector-container">
+                        <div class="w-row">
+                            <div class="padding-none w-col w-col-3" ng-repeat="headerOption in headerOptions.offered">
+                                <div class="info-padding upload-title">@{{ headerOption.value }}</div>
+                            </div>
+                            <div class="padding-none w-col w-col-3">
+                                <div class="info-padding upload-title">Inventory Quantity</div>
+                                <div class="info-container">
+                                    <div class="info-padding">32938847343</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="w-row"  ng-repeat="row in table">
+
+                            <div class="padding-none w-col w-col-3" ng-repeat="headerOption in headerOptions.offered">
+
+                                <div class="info-container">
+                                    <div class="info-padding">@{{ row[headerOption.key] }}</div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </section>
@@ -88,124 +106,5 @@
     </div>
 
 
-    <div ng-controller="StoreController" data-reactroot="" class="Polaris-Page">
-        <div class="Polaris-Page__Content">
-            <div class="Polaris-Layout">
 
-
-
-                <div class="Polaris-Layout__AnnotatedSection">
-                    <div class="Polaris-Layout__AnnotationWrapper">
-
-                        <div class="Polaris-Layout__AnnotationContent">
-                            <h2 class="Polaris-Heading sfui"><div class="oval">1</div>Upload your CSV</h2>
-                            <div class="Polaris-Card">
-                                <div class="Polaris-Card__Section">
-                                    <div class="Polaris-SettingAction">
-                                        <div class="Polaris-SettingAction__Setting">Upload your CSV<br>
-                                            <input type="file" csv-reader="" save-results-callback="readCSV(csv_data)">
-                                        </div>
-
-
-
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="Polaris-Layout__AnnotatedSection">
-                    <div class="Polaris-Layout__AnnotationWrapper">
-                        <div class="Polaris-Layout__AnnotationContent">
-                                <div role="group" class="">
-                                    <div class="">
-
-                                        <div ng-show="fileSelected==true" class="col-sm-3 " style="padding-left: 0px;">
-                                            <h2 class="Polaris-Heading sfui"><div class="oval">2</div>Select SKU</h2>
-                                                    <div class="custom-card">
-
-                                                        <div ng-if="$first"   ng-repeat="headerOption in headerOptions.offered">
-
-
-
-
-
-                                                            <div class="Polaris-Labelled__LabelWrapper">
-                                                                <div class="Polaris-Label">
-                                                                    <label id="TextField13Label" for="TextField13" class="Polaris-Label__Text">@{{ headerOption.value }}<div class="red"> (required)</div> </label>
-                                                                </div>
-                                                            </div>
-
-                                                            <div class="Polaris-Select Polaris-Select--placeholder">
-                                                                <select class="Polaris-Select__Input" aria-invalid="false" ng-model="headerOption.mapped_to" ng-change="updateTable(headerOption)" >
-                                                                    <option label="Select" value="__placeholder__" disabled="" hidden=""></option>
-                                                                    <option ng-repeat="(key, value) in headerOptions.inCSV" value="@{{ key }}">@{{ value }}</option>
-                                                                </select>
-                                                                <div class="Polaris-Select__Icon"><span class="Polaris-Icon"><svg class="Polaris-Icon__Svg" viewBox="0 0 20 20"><path d="M13 8l-3-3-3 3h6zm-.1 4L10 14.9 7.1 12h5.8z" fill-rule="evenodd"></path></svg></span></div>
-                                                                <div class="Polaris-Select__Backdrop"></div>
-                                                            </div>
-
-                                                        </div>
-                                                    </div>
-
-
-                                                </div>
-                                        <div ng-show="skuSelected==true" class="col-sm-9 " style="padding-right: 0px;">
-                                            <h2 class="Polaris-Heading sfui"><div class="oval">3</div>Select fields you would like to update</h2>
-                                            <div class="custom-card">
-                                                <div ng-if="!$first" ng-class="{'col-sm-4': !$first}"  ng-repeat="headerOption in headerOptions.offered">
-
-
-
-
-
-                                                    <div class="Polaris-Labelled__LabelWrapper">
-                                                        <div class="Polaris-Label">
-                                                            <label id="TextField13Label" for="TextField13" class="Polaris-Label__Text">@{{ headerOption.value }}</label>
-                                                        </div>
-                                                    </div>
-
-                                                    <div class="Polaris-Select Polaris-Select--placeholder">
-                                                        <select class="Polaris-Select__Input" aria-invalid="false" ng-model="headerOption.mapped_to" ng-change="updateTable(headerOption)" >
-                                                            <option label="Select" value="__placeholder__" disabled="" hidden=""></option>
-                                                            <option ng-repeat="(key, value) in headerOptions.inCSV" value="@{{ key }}">@{{ value }}</option>
-                                                        </select>
-                                                        <div class="Polaris-Select__Icon"><span class="Polaris-Icon"><svg class="Polaris-Icon__Svg" viewBox="0 0 20 20"><path d="M13 8l-3-3-3 3h6zm-.1 4L10 14.9 7.1 12h5.8z" fill-rule="evenodd"></path></svg></span></div>
-                                                        <div class="Polaris-Select__Backdrop"></div>
-                                                    </div>
-                                            </div>
-                                            </div>
-                                        </div>
-
-                                </div>
-                                </div>
-                        </div>
-                    </div>
-                </div>
-                <div ng-show="showTable==true" class="Polaris-Layout__AnnotatedSection">
-                    <div class="Polaris-Layout__AnnotationWrapper">
-                        <div class="Polaris-Layout__AnnotationContent">
-                            <div class="white-block">
-                                <table class="table table-striped">
-                                    <thead>
-                                    <tr>
-                                        <td class="col-sm-3" ng-repeat="headerOption in headerOptions.offered">@{{ headerOption.value }}</td>
-                                    </tr>
-                                    </thead>
-                                    <tbody>
-                                    <tr ng-repeat="row in table">
-                                        <td  ng-repeat="headerOption in headerOptions.offered">@{{ row[headerOption.key] }}</td>
-                                    </tr>
-                                    </tbody>
-                                </table>
-                            </div>
-                        </div>
-
-                    </div>
-                </div>
-            </div>
-        </div>
-
-    </div>
 @endsection
