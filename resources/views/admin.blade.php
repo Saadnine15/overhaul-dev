3e03fe5ff3ee71b3b01e9b92083671c7c71e2432
Saad Bin Naeem <saad@nine15.com>
2018-03-09 16:00:16 -0600

    better

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -72,15 +72,6 @@
                                         <div ng-if="$first"   ng-repeat="headerOption in headerOptions.offered">
 
 
-
-
-
-                                            <div class="Polaris-Labelled__LabelWrapper">
-                                                <div class="Polaris-Label">
-                                                    <label id="TextField13Label" for="TextField13" class="Polaris-Label__Text">@{{ headerOption.value }}<div class="red"> (required)</div> </label>
-                                                </div>
-                                            </div>
-
                                             <div class="Polaris-Select Polaris-Select--placeholder">
                                                 <select class="Polaris-Select__Input" aria-invalid="false" ng-model="headerOption.mapped_to" ng-change="updateTable(headerOption)" >
                                                     <option label="Select" value="__placeholder__" disabled="" hidden=""></option>
@@ -105,36 +96,6 @@
                                 <div role="group" class="">
                                     <div class="">
 
-                                        <div ng-show="fileSelected==true" class="col-sm-3 " style="padding-left: 0px;">
-                                            <h2 class="Polaris-Heading sfui"><div class="oval">2</div>Select SKU</h2>
-                                                    <div class="custom-card">
-
-                                                        <div ng-if="$first"   ng-repeat="headerOption in headerOptions.offered">
-
-
-
-
-
-                                                            <div class="Polaris-Labelled__LabelWrapper">
-                                                                <div class="Polaris-Label">
-                                                                    <label id="TextField13Label" for="TextField13" class="Polaris-Label__Text">@{{ headerOption.value }}<div class="red"> (required)</div> </label>
-                                                                </div>
-                                                            </div>
-
-                                                            <div class="Polaris-Select Polaris-Select--placeholder">
-                                                                <select class="Polaris-Select__Input" aria-invalid="false" ng-model="headerOption.mapped_to" ng-change="updateTable(headerOption)" >
-                                                                    <option label="Select" value="__placeholder__" disabled="" hidden=""></option>
-                                                                    <option ng-repeat="(key, value) in headerOptions.inCSV" value="@{{ key }}">@{{ value }}</option>
-                                                                </select>
-                                                                <div class="Polaris-Select__Icon"><span class="Polaris-Icon"><svg class="Polaris-Icon__Svg" viewBox="0 0 20 20"><path d="M13 8l-3-3-3 3h6zm-.1 4L10 14.9 7.1 12h5.8z" fill-rule="evenodd"></path></svg></span></div>
-                                                                <div class="Polaris-Select__Backdrop"></div>
-                                                            </div>
-
-                                                        </div>
-                                                    </div>
-
-
-                                                </div>
                                         <div ng-show="skuSelected==true" class="col-sm-9 " style="padding-right: 0px;">
                                             <h2 class="Polaris-Heading sfui"><div class="oval">3</div>Select fields you would like to update</h2>
                                             <div class="custom-card">
