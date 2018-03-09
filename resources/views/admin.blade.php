3314148598d7c2bfda474e03c8f5e5010800d2d1
Saad Bin Naeem <saad@nine15.com>
2018-03-09 16:04:58 -0600

    better

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -90,6 +90,52 @@
                     </div>
                 </div>
 
+                                {{-----------------3 select fields to update-----------------}}
+                <div class="Polaris-Layout__AnnotatedSection">
+                    <div class="Polaris-Card">
+                        <div class="Polaris-Card__Section">
+                            <div class="Polaris-SettingAction">
+                                <div class="Polaris-SettingAction__Setting">
+                                    <div class="Polaris-Stack">
+                                        <div class="Polaris-Stack__Item Polaris-Stack__Item--fill">
+                                            <div class="Polaris-AccountConnection__Content">
+                                                <div><div class="oval">3</div>Select fields you would like to update</div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="Polaris-SettingAction__Action">
+                                    <div class="custom-card">
+                                                <div ng-if="!$first" ng-class="{'col-sm-4': !$first}"  ng-repeat="headerOption in headerOptions.offered">
+
+
+
+
+
+                                                    <div class="Polaris-Labelled__LabelWrapper">
+                                                        <div class="Polaris-Label">
+                                                            <label id="TextField13Label" for="TextField13" class="Polaris-Label__Text">@{{ headerOption.value }}</label>
+                                                        </div>
+                                                    </div>
+
+                                                    <div class="Polaris-Select Polaris-Select--placeholder">
+                                                        <select class="Polaris-Select__Input" aria-invalid="false" ng-model="headerOption.mapped_to" ng-change="updateTable(headerOption)" >
+                                                            <option label="Select" value="__placeholder__" disabled="" hidden=""></option>
+                                                            <option ng-repeat="(key, value) in headerOptions.inCSV" value="@{{ key }}">@{{ value }}</option>
+                                                        </select>
+                                                        <div class="Polaris-Select__Icon"><span class="Polaris-Icon"><svg class="Polaris-Icon__Svg" viewBox="0 0 20 20"><path d="M13 8l-3-3-3 3h6zm-.1 4L10 14.9 7.1 12h5.8z" fill-rule="evenodd"></path></svg></span></div>
+                                                        <div class="Polaris-Select__Backdrop"></div>
+                                                    </div>
+                                                </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+
+
                 <div class="Polaris-Layout__AnnotatedSection">
                     <div class="Polaris-Layout__AnnotationWrapper">
                         <div class="Polaris-Layout__AnnotationContent">
