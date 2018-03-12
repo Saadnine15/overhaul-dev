159011c51dcd21b08d10840bfef6d8cca4a567f7
Saad Bin Naeem <saad@nine15.com>
2018-03-12 12:39:42 -0500

    better

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -197,7 +197,7 @@
                                     </div>
                                 </div>
                                 <div class="Polaris-SettingAction__Action">
-                                    <button ng-disabled="!headerOption.mapped_to"  data-ng-click="updateProducts()" class="Polaris-Button"><span class="Polaris-Button__Content"><span>Update</span></span></button>
+                                    <button ng-disabled="headerOption.mapped_to !='' "  data-ng-click="updateProducts()" class="Polaris-Button"><span class="Polaris-Button__Content"><span>Update</span></span></button>
                                 </div>
                             </div>
 
