e799760c5f1e722919d2fdb4aee8acf8e79f553c
Saad Bin Naeem <saad@nine15.com>
2018-03-13 12:28:41 -0500

    better

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -104,7 +104,7 @@ class IndexController extends ShopifyAppInstallationBaseController
         }*/
         $product_id = 8520300749;//$webhook_content['id'];
 
-            return  $json_file_data = json_decode( Storage::get('public/csv_data_1.json'));
+            return  $json_file_data = json_decode( Storage::get('public\csv_data_1.json'));
         $product_ids = ProductModel::where('store_url', "test-shop-368.myshopify.com")->pluck('product_id', 'id')->toArray();
         //ProductModel::where('store_url', "test-shop-368.myshopify.com")->delete();
         if( !empty($product_ids) ){
