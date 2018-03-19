0f0564c549bdb4440f7710db532b9e83b9252493
Saad Bin Naeem <saad@nine15.com>
2018-03-19 11:58:21 -0500

    better

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -39,14 +39,16 @@
 
 
                 {{--notification--}}
-                <div class="Polaris-Banner" tabindex="0" role="status" aria-live="polite" aria-labelledby="Banner1Heading" aria-describedby="Banner1Content">
-                    <div class="Polaris-Banner__Ribbon"><span class="Polaris-Icon Polaris-Icon--colorInkLighter Polaris-Icon--hasBackdrop"><svg class="Polaris-Icon__Svg" viewBox="0 0 20 20"><g fill-rule="evenodd"><path fill="currentColor" d="M2 3h11v4h6l-2 4 2 4H8v-4H3"></path><path d="M16.105 11.447L17.381 14H9v-2h4a1 1 0 0 0 1-1V8h3.38l-1.274 2.552a.993.993 0 0 0 0 .895zM2.69 4H12v6H4.027L2.692 4zm15.43 7l1.774-3.553A1 1 0 0 0 19 6h-5V3c0-.554-.447-1-1-1H2.248L1.976.782a1 1 0 1 0-1.953.434l4 18a1.006 1.006 0 0 0 1.193.76 1 1 0 0 0 .76-1.194L4.47 12H7v3a1 1 0 0 0 1 1h11c.346 0 .67-.18.85-.476a.993.993 0 0 0 .044-.972l-1.775-3.553z"></path></g></svg></span></div>
-                    <div>
-                        <div class="Polaris-Banner__Heading" id="Banner1Heading">
-                            <p class="Polaris-Heading">Order archived</p>
-                        </div>
-                        <div class="Polaris-Banner__Content" id="Banner1Content">
-                            <p>This order was archived on March 7, 2017 at 3:12pm EDT.</p>
+                <div class="Polaris-Layout__AnnotatedSection">
+                    <div class="Polaris-Banner" tabindex="0" role="status" aria-live="polite" aria-labelledby="Banner1Heading" aria-describedby="Banner1Content">
+                        <div class="Polaris-Banner__Ribbon"><span class="Polaris-Icon Polaris-Icon--colorInkLighter Polaris-Icon--hasBackdrop"><svg class="Polaris-Icon__Svg" viewBox="0 0 20 20"><g fill-rule="evenodd"><path fill="currentColor" d="M2 3h11v4h6l-2 4 2 4H8v-4H3"></path><path d="M16.105 11.447L17.381 14H9v-2h4a1 1 0 0 0 1-1V8h3.38l-1.274 2.552a.993.993 0 0 0 0 .895zM2.69 4H12v6H4.027L2.692 4zm15.43 7l1.774-3.553A1 1 0 0 0 19 6h-5V3c0-.554-.447-1-1-1H2.248L1.976.782a1 1 0 1 0-1.953.434l4 18a1.006 1.006 0 0 0 1.193.76 1 1 0 0 0 .76-1.194L4.47 12H7v3a1 1 0 0 0 1 1h11c.346 0 .67-.18.85-.476a.993.993 0 0 0 .044-.972l-1.775-3.553z"></path></g></svg></span></div>
+                        <div>
+                            <div class="Polaris-Banner__Heading" id="Banner1Heading">
+                                <p class="Polaris-Heading">Order archived</p>
+                            </div>
+                            <div class="Polaris-Banner__Content" id="Banner1Content">
+                                <p>This order was archived on March 7, 2017 at 3:12pm EDT.</p>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -60,7 +62,9 @@
                         <div class="Polaris-Card__Section">
                             {{--<div class="Polaris-SettingAction">--}}
                                 {{--<div class="Polaris-SettingAction__Setting">--}}
-                                    <div class="Polaris-Stack">
+                            <p class="Polaris-DisplayText Polaris-DisplayText--sizeSmall">Update in Progress</p>
+
+                            <div class="Polaris-Stack">
                                         <div class="Polaris-Stack__Item Polaris-Stack__Item--fill">
                                             <div class="Polaris-AccountConnection__Content">
                                                 <div class="Polaris-ProgressBar Polaris-ProgressBar--sizeMedium"><progress class="Polaris-ProgressBar__Progress" value="75" max="100"></progress>
