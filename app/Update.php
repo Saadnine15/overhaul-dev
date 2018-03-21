1194c081e2503df3d5636040581b44cf062c5d48
Saad Bin Naeem <saad@nine15.com>
2018-03-21 10:57:01 -0500

    better

--- app/Update.php
+++ app/Update.php
@@ -15,6 +15,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Update extends Model{
     use SoftDeletes;
     protected $fillable = [
+        'update_id',
         'old_qty',
         'new_qty',
         'old_price',
