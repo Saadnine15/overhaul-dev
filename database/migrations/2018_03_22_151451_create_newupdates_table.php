af5c55554260d62e1519e389a493b4d75206fae9
Saad Bin Naeem <saad@nine15.com>
2018-03-22 10:16:06 -0500

    better

---
+++ database/migrations/2018_03_22_151451_create_newupdates_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNewupdatesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trackings', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('store_name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
