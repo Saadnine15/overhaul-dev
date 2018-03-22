dd428385eb2981c985eaa557b2fc31b2bb9bc900
Saad Bin Naeem <saad@nine15.com>
2018-03-22 15:47:51 -0500

    better

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -167,7 +167,7 @@ class IndexController extends ShopifyAppInstallationBaseController
     }
 
     public function getRecords(){
-        return Job::all();
+        return JobsFailed::all();
         return Record::where('id','>',0)->get();
     }
     public function checkJobStatus(){
