ed20a9b9c349a46809324465c3ec3d45fad081bb
Saad Bin Naeem <saad@nine15.com>
2018-03-23 12:04:54 -0500

    better

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,7 +20,7 @@ class AppServiceProvider extends ServiceProvider
         Queue::after(function ($event) {
             $command = unserialize($event->data['data']['command']);
 //            $store_settings = $command->store_settings;
-            var_dump($command->store_settings);
+            var_dump($command->store_settings->store_name);
             exit;
 //            dd($event);
 //            Tracking::create(['store_name',$event]);
