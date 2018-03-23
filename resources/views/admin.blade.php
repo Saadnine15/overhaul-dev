fbc94b6ec3561702f1b2fadf0e508c7b5e563910
Saad Bin Naeem <saad@nine15.com>
2018-03-23 13:17:14 -0500

    better

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -20,7 +20,7 @@
                 {{--dashboard intro--}}
                 <div class="Polaris-EmptyState">
                     <div class="Polaris-EmptyState__Section">
-                        <div class="Polaris-EmptyState__DetailsContainer" style="width: 98%;">
+                        <div class="Polaris-EmptyState__DetailsContainer" >
                             <h1 class="Polaris-Heading">Dashboard</h1>
                             <span class="Polaris-TextStyle--variationSubdued"> Welcome to Overhaul,
                                 we make bulk product updates quick and easy.
@@ -39,7 +39,7 @@
 
                     <div class="Polaris-EmptyState progressing">
                         <div class="Polaris-EmptyState__Section">
-                            <div class="Polaris-EmptyState__DetailsContainer">
+                            <div class="Polaris-EmptyState__DetailsContainer" style="width: 98%;">
 
                                 <div class="Polaris-Banner" tabindex="0" role="status" aria-live="polite"
                                      aria-labelledby="Banner1Heading" aria-describedby="Banner1Content">
